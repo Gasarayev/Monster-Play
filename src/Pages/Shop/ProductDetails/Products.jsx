3e7fdf777a41ec4fdb6b928c625785a19a561f9c
Gasarayev <gasarayev.emil@gmail.com>
2024-04-08 17:13:42 +0400

    shop products details page

--- src/Pages/Shop/ProductDetails/Products.jsx
+++ src/Pages/Shop/ProductDetails/Products.jsx
@@ -1,17 +1,110 @@
-// Products.jsx
-import React from 'react';
+
+import React, { useState } from 'react';
+import '../shop_css/products.css'
 
 function Products({ product }) {
+
+  const [quantity, setQuantity] = useState(1);
+
+  const handleInputChange = (event) => {
+
+    if (event.target.value < 1) {
+      setQuantity(1);
+    } else {
+      setQuantity(event.target.value);
+    }
+  };
+
   return (
-    <div className="product-container">
+    <div className="product">
+      <div className='product_container'>
+        <h5>{product.title}</h5>
+        <div className='product_imgPart'>
+          <div className='product_img'>
+            <img src={product.img} alt={product.title} />
+          </div>
+
+          <div className='product_content'>
+            <h1>
+              {product.title}
+            </h1>
+
+            {product.stars == 4 && (
+              <div className="product_starsIcon">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+              </div>
+            )}
+
+            {product.stars == 5 && (
+              <div className="product_starsIcon">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+              </div>
+            )}
+
+            <p className='product_price'>
+              {product.newPrice}
+            </p>
+
+            <p>
+              {product.description}
+            </p>
+
+            <div className='product_count_part'>
+              <input type="number" value={quantity} onChange={handleInputChange} />
+              <button>ADD TO CART</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='product_desc_info'>
+
+          <div className='product_description'>
+            <h5>description</h5>
+            <p className='product_description_text'>
+              {product.desc}
+            </p>
+            <p className='product_description_text'>
+              {product.desc2}
+            </p>
+            <p className='product_description_text'>
+              {product.desc3}
+            </p>
+            <p className='product_description_text'>
+              {product.desc4}
+            </p>
+          </div>
+
+          <div className='product_information'>
+            <h5>additional information</h5>
+            <div className='product_infoContain'>
+              <div className='product_informationType'>
+                <p>Colour</p>
+                <p>Headphone Fit</p>
+                <p>Item Dimension</p>
+                <p>Item Weight</p>
+                <p>Special Feature</p>
+              </div>
+              <div className='product_informationPro'>
+                <p>{product.colour}</p>
+                <p>{product.headphoneFit}</p>
+                <p>{product.ItemDimension}</p>
+                <p>{product.itemWeight}</p>
+                <p>{product.SpecialFeature}</p>
+              </div>
+            </div>
+          </div>
 
 
-        
-      <h2>{product.title}</h2>
-      <img src={product.img} alt={product.title} />
-      <p>{product.newPrice}</p>
-      <p>{product.desc}</p>
-    
+        </div>
+
+      </div>
     </div>
   );
 }
